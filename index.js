722214d8d4b907789e389fe5a044a0e23be79537
André Antunes Vieira <andreantunesv@gmail.com>
2017-07-20 16:49:08 -0300

    Version 1.0.2 - fix ES5 problems

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if(typeof(Number.prototype.pad) === 'undefined'){
   Number.prototype.pad = function(n) {
-    if(n === undefined) n = 2;
+    if(typeof(n) === 'undefined') n = 2;
     var zeros = new Array(n).join('0');
     return (zeros + this).slice(-n);
   };
